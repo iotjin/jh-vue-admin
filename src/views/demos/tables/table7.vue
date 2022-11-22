c1f0b47d875055905aca4460a9bf5482537e7f15
iotjin <179608047@qq.com>
2022-11-22 16:25:33 +0800

    更新-demos

--- src/views/demos/tables/table7.vue
+++ src/views/demos/tables/table7.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="app-container">
 
-    <el-table :data="singleOrderData" border :span-method="arraySpanMethod" :header-cell-style="headerCellStyle" :cell-style="cellStyle" @selection-change="selectionLineChangeHandle">
+    <el-table ref="tableRef" :data="singleOrderData" border :span-method="arraySpanMethod" :header-cell-style="headerCellStyle" :cell-class-name="cellClassName" :cell-style="cellStyle" @selection-change="selectionChangeHandle">
       <el-table-column v-if="isShowCheckbox" type="selection" width="55" />
-      <el-table-column label="ProductPlaceholder" prop="productPlaceholder" width="150" />
+      <!-- productName use productPlaceholder field  -->
+      <el-table-column label="Product" prop="productPlaceholder" width="150" />
       <el-table-column label="Product" prop="productName" align="center" width="600" />
       <el-table-column label="Quantity" prop="quantity" align="center" width="100" />
       <el-table-column label="Price" align="center" width="150">
@@ -49,53 +50,63 @@ export default {
     }
   },
   created() {
-    this.singleOrderData = [
-      {
-        productId: '1',
-        productName: 'testCombo',
-        discount: 0,
-        quantity: 1,
-        price: 200,
-        subTotal: 200,
-        discountNotes: 'Combo Discount',
-        memberProductDiscount: 397,
-        productMemberList: [
-          {
-            productId: '1-1',
-            productName: 'testCombo - child1',
-            quantity: 1,
-            price: 499,
-            subTotal: 200,
-            discount: 331.83,
-            discountNotes: 'Combo Discount'
-          },
-          {
-            productId: '1-2',
-            productName: 'testCombo - child2',
-            quantity: 2,
-            price: 49,
-            subTotal: 400,
-            discount: 32.58,
-            discountNotes: 'Combo Discount'
-          }
-        ]
-      },
-      {
-        productId: '2',
-        productName: 'product',
-        discount: 0,
-        quantity: 1,
-        price: 89,
-        subTotal: 89,
-        discountNotes: '',
-        memberProductDiscount: 0,
-        productMemberList: []
-      }
-    ]
-
+    this.testData()
     this.singleOrderData = this.handleData(this.singleOrderData)
   },
+  mounted() {
+    if (this.isShowCheckbox && this.singleOrderData.length) {
+      this.$refs.tableRef.toggleAllSelection()
+      // this.$nextTick(() => {
+      //   this.$refs.tableRef.toggleAllSelection()
+      // })
+    }
+  },
   methods: {
+    testData() {
+      this.singleOrderData = [
+        {
+          productId: '1',
+          productName: 'testCombo',
+          discount: 0,
+          quantity: 1,
+          price: 200,
+          subTotal: 200,
+          discountNotes: 'Combo Discount',
+          memberProductDiscount: 397,
+          productMemberList: [
+            {
+              productId: '1-1',
+              productName: 'testCombo - child1',
+              quantity: 1,
+              price: 499,
+              subTotal: 200,
+              discount: 331.83,
+              discountNotes: 'Combo Discount'
+            },
+            {
+              productId: '1-2',
+              productName: 'testCombo - child2',
+              quantity: 2,
+              price: 49,
+              subTotal: 400,
+              discount: 32.58,
+              discountNotes: 'Combo Discount'
+            }
+          ]
+        },
+        {
+          productId: '2',
+          productName: 'product',
+          discount: 0,
+          quantity: 1,
+          price: 89,
+          subTotal: 89,
+          discountNotes: '',
+          memberProductDiscount: 0,
+          productMemberList: []
+        }
+      ]
+    },
     handleData(singleOrderData) {
       var newArr = []
       for (let i = 0; i < singleOrderData.length; i++) {
@@ -103,6 +114,7 @@ export default {
         const product = singleOrderData[i]
         const newProduct = { ...singleOrderData[i] }
         delete newProduct.productMemberList
+        newProduct.isNormalProduct = !product.productMemberList.length
         newArr.push(newProduct)
         if (product.productMemberList && product.productMemberList.length) {
           for (let j = 0; j < product.productMemberList.length; j++) {
@@ -117,16 +129,16 @@ export default {
       }
       return newArr
     },
-    selectionLineChangeHandle(val) {
+    selectionChangeHandle(val) {
       this.selectionList = val
       console.log(JSON.stringify(this.selectionList))
     },
     // 设置表头样式 - 合并表头
     headerCellStyle({ row, column, rowIndex, columnIndex }) {
       if (row[0].level === 1) {
-        row[0].colSpan = 0
+        // row[0].colSpan = 0
         row[1].colSpan = 2
-
+        row[2].colSpan = 0
         const tempColumn = this.isShowCheckbox ? 2 : 0
         if (columnIndex === tempColumn) {
           return { display: 'none' }
@@ -139,6 +151,16 @@ export default {
     cellStyle({ row, column, rowIndex, columnIndex }) {
       return { textAlign: 'center' }
     },
+    // 隐藏Checkbox
+    cellClassName({ row, column, rowIndex, columnIndex }) {
+      if (columnIndex === 0) {
+        if (row.isNormalProduct || row.isChildren) {
+          //
+        } else {
+          return 'disabled-column'
+        }
+      }
+    },
     // 合并列
     arraySpanMethod({ row, column, rowIndex, columnIndex }) {
       // console.log('rowIndex=' + rowIndex + '-----' + 'columnIndex=' + columnIndex)
@@ -150,17 +172,17 @@ export default {
       // header
 
       // father
-      if (column['label'] === 'ProductPlaceholder' && !row.isChildren) {
+      if (column['property'] === 'productPlaceholder' && !row.isChildren) {
         return [1, 2]
       }
-      if (column['label'] === 'Product' && !row.isChildren) {
+      if (column['property'] === 'productName' && !row.isChildren) {
         return [0, 0]
       }
       // children
-      if (column['label'] === 'ProductPlaceholder' && row.isChildren && row.isStart) {
+      if (column['property'] === 'productPlaceholder' && row.isChildren && row.isStart) {
         return [row.row, 1]
       }
-      if (column['label'] === 'ProductPlaceholder' && row.isChildren && !row.isStart) {
+      if (column['property'] === 'productPlaceholder' && row.isChildren && !row.isStart) {
         return [0, 0]
       }
 
@@ -183,4 +205,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+::v-deep {
+  .disabled-column .el-checkbox__input {
+    display: none;
+  }
+}
 </style>
