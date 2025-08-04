a13061614e04debebdac3d234fa6aa6a1a267897
iotjin <179608047@qq.com>
2025-08-04 16:17:33 +0800

    更新-demos

--- vue.config.js
+++ vue.config.js
@@ -17,7 +17,7 @@ const name = defaultSettings.title || '后台管理系统' // page title
 // For example, Mac: sudo npm run
 // You can change the port by the following methods:
 // port = 9528 npm run dev OR npm run dev --port = 9528
-const port = process.env.port || process.env.npm_config_port || 3529 // dev port
+const port = process.env.port || process.env.npm_config_port || 9528 // dev port
 
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
